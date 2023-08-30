739a804280ece2e4fc70fa3b11c23196f9d17fc6
Mermaido <ishitaraina18@gmail.com>
2023-08-30 22:44:49 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,7 @@ import sun from '../images/sun.svg';
 import moon from '../images/moon.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import cross from '../images/cross.svg';
 
 const ThemeToggler = () => {
     const [theme, setTheme] = useState('light');
@@ -19,27 +20,33 @@ const ThemeToggler = () => {
 
     const handleEnterPress = (e) => {
         if (e.key === 'Enter' && inputValue.trim() !== '') {
-            setItems([...items, inputValue]);
+            setItems([...items, { text: inputValue, checked: false }]);
             setInputValue('');
         }
     };
 
-    // Define background color classes for light and dark themes
-    const inputBgClass = theme === 'dark' ? 'bg-darkgreyblue' : 'bg-gray-100';
-    const inputTextClass = theme === 'dark' ? 'text-lightgrey' : 'text-darkgreyblue';
+    const handleCheckboxChange = (index) => {
+        const updatedItems = [...items];
+        updatedItems[index].checked = !updatedItems[index].checked;
+        setItems(updatedItems);
+    };
 
-    // Set a fixed height for the container
+    const handleDeleteItem = (index) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+    };
+
+    const inputBgClass = theme === 'dark' ? 'bg-darkgreyblue' : 'bg-gray-100';
+    const inputTextClass = theme === 'dark' ? 'text-light-grey-blue' : 'text-darkgreyblue';
     const containerStyle = {
-        height: '300px', // Adjust the height as needed
+        height: '300px',
     };
 
     document.documentElement.className = theme === 'dark' ? 'dark-theme' : 'light-theme';
 
     useEffect(() => {
-        // Listen for Enter key press globally
         window.addEventListener('keydown', handleEnterPress);
-
-        // Remove the event listener when the component unmounts
         return () => {
             window.removeEventListener('keydown', handleEnterPress);
         };
@@ -57,32 +64,46 @@ const ThemeToggler = () => {
                 <div className={`relative mt-4 rounded-md shadow-sm item-center`}>
                     <FontAwesomeIcon
                         icon={faCircle}
-                        className="text-gray-600 absolute inset-y-0 left-0 m-4 text-lg" 
+                        className="text-gray-600 absolute inset-y-0 left-0 m-4 text-lg"
                     />
                     <input
                         type="text"
                         name="todo"
                         id="todo"
-                        className={`input ${inputBgClass} block rounded-md border-0 py-1.5 pl-12 pr-20 ${inputTextClass} placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus: sm:text-base sm:leading-6`}
+                        className={`input-box ${inputBgClass} block rounded-sm border-0 py-1.5 pl-12 pr-20 ${inputTextClass} placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus: sm:text-base sm:leading-6`}
                         placeholder="Create a new todo..."
                         value={inputValue}
                         onChange={handleInputChange}
-                        onKeyDown={handleEnterPress} 
+                        onKeyDown={handleEnterPress}
                     />
                 </div>
             </div>
             <div className='flex items-center justify-center'>
                 <div className={`relative mt-4 rounded-md shadow-sm`}>
-                    {items.map((item, index) => (
-                        <span
-                            key={index}
-                            className={`input ${inputBgClass} block  border py-1.5 pl-12 pr-20 ${inputTextClass} placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus: sm:text-base sm:leading-6`}
-                        >
-                            {item}
-                        </span>
-                    ))}
+
+                {items.map((item, index) => (
+    <div key={index} className={`input-box ${inputBgClass} flex items-center box-container border rounded-sm py-3.5 pl-4 pr-4 ${inputTextClass} ${item.checked ? 'line-through text-gray-400' : ''}`}>
+        <label className="flex items-center">
+            <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={() => handleCheckboxChange(index)
+                }
+            />
+            <span className="checkmark"></span>
+        </label>
+        <span className="mr-4">{item.text}</span>
+        <img
+            src={cross}
+            onClick={() => handleDeleteItem(index)}
+            className="w-4 h-4 ml-auto cursor-pointer"
+        />
+    </div>
+))}
+
                 </div>
             </div>
+
         </div>
     );
 };
