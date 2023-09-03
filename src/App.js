b838c82063ddaffd0bc551161ecbd6f10234c88c
Mermaido <ishitaraina18@gmail.com>
2023-09-03 21:38:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Todo from './components/Todo';
-import AuthProvider from './AuthProvider'; 
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import AuthProvider, { AuthContext } from './AuthProvider';  
+import { ThemeProvider } from './ThemeContext'; 
 
 function App() {
   return (
     <div>
       <div className='app'>
+      <ThemeProvider>
         <AuthProvider> 
           <Routes>
-            <Route path="/" element={<Todo />} />
+            <Route path="/todo-list" element={<Todo />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
           </Routes>
         </AuthProvider>
+        </ThemeProvider>
       </div>
     </div>
   );
