fc6d77a1639a4ebb3cd994add8c5c0f7940041a3
Mermaido <ishitaraina18@gmail.com>
2023-09-03 21:40:26 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,16 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { AuthContext } from "../AuthProvider"; 
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { AuthContext } from "../AuthContext";
 import { auth } from "../firebase";
+import { useTheme } from "../ThemeContext";
+import sun from '../images/sun.svg';
+import moon from '../images/moon.svg';
 
-const LogIn = () => {
-    const navigate = useNavigate();
-    const { dispatch } = useContext(AuthContext);
+const Login = () => {
+  const { dispatch } = useContext(AuthContext); 
+  const navigate = useNavigate();
+  const { isDarkTheme, toggleTheme } = useTheme();
 
   const [values, setValues] = useState({
     email: "",
@@ -16,89 +19,120 @@ const LogIn = () => {
 
   const [errorMsg, setErrorMsg] = useState("");
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
-    if (values.email === "" || values.password === "") {
+    if (values.name === "" || values.email === "" || values.password === "") {
+      setErrorMsg("Both email and password are required.");
       return;
     }
 
-    setErrorMsg("");
+    try {
+     
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password,
+        values.name
+      );
 
-    signInWithEmailAndPassword(auth, values.email, values.password)
-      .then(async (userCredential) => {
-        const user = userCredential.user;
-        console.log("User logged in with credentials : ",user);
-        dispatch({ type: "LOGIN", payload: user });
-        navigate("/quizzes");
-      })
-      .catch((e) => {
-        console.error("Error logging in : ",e)
-        setErrorMsg("Error logging in");
-      });
+      const user = userCredential.user;
+
+   
+      dispatch({ type: "LOGIN", payload: user });
+      navigate("/search", { state: { name: values.name } });
+    } catch (e) {
+      console.error("Error logging in: ", e);
+      setErrorMsg("Invalid email or password. Please try again.");
+    }
   };
+  
 
   return (
-    <div className="p-6 text-white flex justify-center items-center min-h-screen">
-      <div className="flex justify-center items-center h-full">
-        <div className="w-80 bg-custom-red p-6 rounded shadow">
-          <h1 className="text-white text-2xl font-bold  mb-6 text-center">Login</h1>
-          <form onSubmit={handleLogin}>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-white font-bold mb-2">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full bg-white text-custom-black rounded px-3 py-2 focus:outline-0"
-                value={values.email}
-                onChange={(event) =>
-                  setValues((prev) => ({ ...prev, email: event.target.value }))
-                }
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block text-white font-bold mb-2">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full bg-white text-custom-black rounded px-3 py-2 focus:outline-0"
-                value={values.password}
-                onChange={(event) =>
-                  setValues((prev) => ({
-                    ...prev,
-                    password: event.target.value,
-                  }))
-                }
-                required
-              />
-            </div>
-            <div className="flex justify-center items-center text-xs my-2">
-              <span className="text-gray-600">Don't have an account?</span>&nbsp;&nbsp;
-              <span
-                className="text-white font-bold cursor-pointer"
-                onClick={() => navigate("/signup")}
-              >
-                Sign Up
-              </span>
-            </div>
-            {errorMsg && <p className="text-red-500 mt-2 mb-2 text-sm flex justify-center">{errorMsg}</p>}
-
-            <button
-              type="submit"
-              className="w-full bg-custom-black text-white rounded py-2 hover:bg-white hover:text-custom-black"
+    <div className="bgtheme">
+<div className="auth-container">
+<h1 className="text-2xl font-bold text-center">Login</h1>
+      <button onClick={toggleTheme}>
+  <img src={isDarkTheme ? sun : moon} alt={`Switch to ${isDarkTheme ? 'Light' : 'Dark'} Theme`} />
+</button>
+</div>
+      
+      <div className={`p-6 text-${isDarkTheme ? 'white' : 'black'} flex justify-center items-center min-h-screen`}>
+      <div className={`flex justify-center items-center h-full`}>
+        <div className={`w-80 bg-${isDarkTheme ? 'black' : 'white'} p-6 rounded shadow`}>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-white font-bold mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+              value={values.name}
+              onChange={(event) =>
+                setValues((prev) => ({ ...prev, name: event.target.value }))
+              }
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-white font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+              value={values.email}
+              onChange={(event) =>
+                setValues((prev) => ({ ...prev, email: event.target.value }))
+              }
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-white font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full bg-white text-customblack rounded px-3 py-2 focus:outline-0"
+              value={values.password}
+              onChange={(event) =>
+                setValues((prev) => ({
+                  ...prev,
+                  password: event.target.value,
+                }))
+              }
+              required
+            />
+          </div>
+          <div className="flex justify-center items-center text-sm my-2">
+            <span className="text-gray-800">Don't have an account?</span>
+            &nbsp;&nbsp;
+            <span
+              className="text-white font-bold cursor-pointer hover:underline"
+              onClick={() => navigate("/signup")}
             >
-              Login
-            </button>
-          </form>
-        </div>
+              Sign Up
+            </span>
+          </div>
+          {errorMsg && <p className="text-red-300 mt-2 mb-2 text-sm flex justify-center">{errorMsg}</p>}
+
+          <button
+            type="submit"
+            className="w-full bg-white text-darkblue rounded py-2 hover:text-white hover:bg-lightblue"
+          >
+           Login
+          </button>
+        </form>
       </div>
+    </div>
+  </div>
     </div>
   );
 };
 
-export default LogIn;
+export default Login;
